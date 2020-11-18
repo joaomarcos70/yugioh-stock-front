3943e7d706f3ce927cde5e83e0765547f5235935
joão <joaomarcos70outlook.com>
2020-11-18 00:18:18 -0300

    ajustando tamanho das cartas na lista

--- src/views/Wants.vue
+++ src/views/Wants.vue
@@ -1,21 +1,25 @@
 <template>
   <v-item-group>
-    <v-container>
-      <v-row>
-        <v-col v-for="n in 7" :key="n" cols="12" md="2">
-          <v-item style="height:18vw" v-slot="{ active, toggle }">
-            <v-card
-              :color="active ? 'primary' : ''"
-              class="d-flex align-center"
-              dark
-              height="200"
-              @click="toggle"
-            >
-              <v-scroll-y-transition>
-                <div v-if="active" class="display-3 flex-grow-1 text-center">
-                  Active
-                </div>
-              </v-scroll-y-transition>
+    <v-container fluid>
+      <v-row >
+        <v-col v-for="card in listaCard" :key="card" cols="12" md="2">
+          <v-item>
+            <v-card>
+              <v-img position="top" height="218" :src="card.img"> </v-img>
+
+              <v-card-actions>
+                <span class="mr-auto font-weight-bold">
+                  {{ card.raridade }}
+                </span>
+
+                <span class="mr-auto font-weight-bold">
+                  {{ card.quantidade }}
+                </span>
+
+                <span class="mr-auto font-weight-bold">
+                  {{ card.idioma }}
+                </span>
+              </v-card-actions>
             </v-card>
           </v-item>
         </v-col>
@@ -25,7 +29,50 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      listaCard: [
+        {
+          img: "https://ygoprodeck.com/pics/13241004.jpg",
+          raridade: "super",
+          quantidade: 3,
+          idioma: "en",
+        },
+        {
+          img: "https://ygoprodeck.com/pics/13241004.jpg",
+          raridade: "super",
+          quantidade: 3,
+          idioma: "en",
+        },
+        {
+          img: "https://ygoprodeck.com/pics/13241004.jpg",
+          raridade: "super",
+          quantidade: 3,
+          idioma: "en",
+        },
+          {
+          img: "https://ygoprodeck.com/pics/13241004.jpg",
+          raridade: "super",
+          quantidade: 3,
+          idioma: "en",
+        },
+          {
+          img: "https://ygoprodeck.com/pics/13241004.jpg",
+          raridade: "super",
+          quantidade: 3,
+          idioma: "en",
+        },
+          {
+          img: "https://ygoprodeck.com/pics/13241004.jpg",
+          raridade: "super",
+          quantidade: 3,
+          idioma: "en",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
